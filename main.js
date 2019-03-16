44268541d0db7b2af4d73ce0794c98c212f16c07
bssrdf <merlintiger@hotmail.com>
2019-03-16 11:27:39 -0400

    more js data type learning

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
 let age = 30;
 age = 31;
-console.log(age)
+console.log(age);
+
+var origNum = "23";
+
+console.log(origNum);
+
+var newNum = age + origNum
+
+console.log(newNum);
+
+const x = null;
+
+const y = undefined;
+
+var l;
+
+console.log(typeof x);
+
+console.log(typeof y);
+
+console.log(typeof l);
+
+const s ='tech, computer, it, code';
+
+console.log(s.split(','));
+
+const numbers = new Array(1, 2, 3, 4, 5);
+
+const nums = [1, 3, 4, 6, 9];
+
+console.log(numbers);
+
+console.log(nums);
+
+const fruits = ['apples', 'oranges', 'pears'];
+
+console.log(fruits);
+
+fruits.push('mongo');
+
+console.log(fruits);
+
+const person = { firstName: 'John', lastName:'Doe', age: 30};
+
+console.log(person);
+
+console.log(typeof person);

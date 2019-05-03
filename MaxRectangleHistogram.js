8dcb7b1f7c35d1b1f76aa54b843e93f4def2450c
bssrdf <bssrdf@gmail.com>
2019-05-02 21:34:36 -0400

    O(n) solution using stack added

--- MaxRectangleHistogram.js
+++ MaxRectangleHistogram.js
@@ -23,4 +23,41 @@ function maxRectInHist(histArr) {
   return maxRectArea;
 }
 
+function maxRectInHistOn(histArr) {
+    const stack = [];
+           
+    let maxRectArea = 0;
+    let top;
+    let currentArea;
+        
+    let i = 0;
+    while (i < histArr.length - 1) {
+      if (!stack.length || histArr[stack[stack.length-1]] <= histArr[i]) {
+        stack.push(i);
+        i++;
+      } else {
+        top = stack[stack.length-1];
+        stack.pop();
+  
+        currentArea = histArr[top] * (!stack.length ? i : i - stack[stack.length-1] - 1);
+  
+        if (maxRectArea < currentArea) {
+          maxRectArea = currentArea;
+        }
+      }
+    }
+      
+    while (stack.length) {
+      top = stack[stack.length-1];
+      stack.pop();
+      currentArea = histArr[top] * (!stack.length ? i : i - stack[stack.length-1] - 1);
+  
+      if (maxRectArea < currentArea) {
+        maxRectArea = currentArea;
+      }
+    }
+  
+    return maxRectArea;
+}
+
 console.log(maxRectInHist(histArr));
